75c6b027bf432cb214ab717c9bca2555333fded5
awadih <47864922+awadih@users.noreply.github.com>
2025-04-21 14:51:22 +0000

    update

--- digital-clock-with-React/src/App.js
+++ digital-clock-with-React/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import request from 'superagent';
-
+import request, { eventNames } from 'superagent';
 
 class App extends Component {
   constructor(props) {
